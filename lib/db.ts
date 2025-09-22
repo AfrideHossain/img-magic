5e4bb6c0c3730aa8477050af6435ca808759acba
AfrideHossain <afredehossain@gmail.com>
2025-09-22 22:26:02 +0600

    Added: Database connection

---
+++ lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { CachedRouteKind } from "next/dist/server/response-cache";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define mongodb_uri in env variables.");
+}
+
+let cachedConnection = global.mongoose;
+
+if (!cachedConnection) {
+  cachedConnection = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cachedConnection.conn) {
+    return cachedConnection.conn;
+  }
+
+  if (!cachedConnection.promise) {
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+  }
+
+  try {
+    cachedConnection.conn = await cachedConnection.promise;
+  } catch (error) {
+    cachedConnection.promise = null;
+    throw error;
+  }
+
+  return cachedConnection.conn;
+}
